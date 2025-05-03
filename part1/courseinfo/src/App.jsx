0d997732293deadef86c4d529e944788c8c31dae
Aleji0309 <78771441+Aleji0309@users.noreply.github.com>
2025-05-02 18:15:12 -0600

    Code Corrected

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,40 +1,52 @@
 const Header = (props) => {
-  console.log(props)
-
-  return(
+  return (
     <>
-      <p>Course Name: {props.name} </p>
+      <p>Course Name: {props.course} </p>
     </>
-  )
-}
+  );
+};
 
+const Content = ({ division, exercises }) => {
+  return (
+    <>
+      <p>Content: {division} Number of Excerices: {exercises} </p>
+      
+    </>
+  );
+};
 
+const Total = ({ total }) => {
+  return <> <p>Total: {total} </p> </>;
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+
+  const division = {
+    part1: "Fundamentals of React",
+    part2: "Using props to pass data",
+    part3: "State of a component",
+  };
+
+  const exercises = {
+    exercises1: 10,
+    exercises2: 7,
+    exercises3: 14,
+  };
+
+  const total = exercises.exercises1 + exercises.exercises2 + exercises.exercises3;
 
   return (
     <div>
-      
-      <Header name={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
+      <Header course={course} />
 
-export default App
+      <Content division={division.part1} exercises={exercises.exercises1} />
+      <Content division={division.part2} exercises={exercises.exercises2} />
+      <Content division={division.part3} exercises={exercises.exercises3} />
+
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default App;
